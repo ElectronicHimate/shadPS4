44a1d3548e62f25c07d79936bc882bc85d60c2d0
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2025-07-27 09:38:30 +1000

    Update address_space.cpp

--- src/core/address_space.cpp
+++ src/core/address_space.cpp
@@ -74,12 +74,6 @@ struct AddressSpace::Impl {
                                                           MEM_RESERVE | MEM_RESERVE_PLACEHOLDER,
                                                           PAGE_NOACCESS, &param, 1));
             if (virtual_base) {
-                break;
-            }
-            virtual_size -= ReductionOnFail;
-        }
-        ASSERT_MSG(virtual_base, "Unable to reserve virtual address space: {}",
-                   Common::GetLastErrorMsg());
 
         system_reserved_base = reinterpret_cast<u8*>(SYSTEM_RESERVED_MIN);
         system_reserved_size = SystemReservedSize;
