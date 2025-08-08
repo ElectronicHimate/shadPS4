15c66400b6d8da3f0f344227c0ee17ca59316711
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2025-08-08 12:23:14 +1000

    Update address_space.cpp

--- src/core/address_space.cpp
+++ src/core/address_space.cpp
@@ -67,12 +67,10 @@ struct AddressSpace::Impl {
                                                           MEM_RESERVE | MEM_RESERVE_PLACEHOLDER,
                                                           PAGE_NOACCESS, &param, 1));
             if (virtual_base) {
-                break;
             }
             reduction += ReductionOnFail;
-        }
-        ASSERT_MSG(virtual_base, "Unable to reserve virtual address space!");
-
+            }
+        
         // Take the reduction off of the system managed area, and leave the others unchanged.
         system_managed_base = virtual_base;
         system_managed_size = SystemManagedSize - reduction;
