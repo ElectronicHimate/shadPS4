03b57641ed0c65883c8bad7ace4e4ee94de3e81d
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2025-08-09 11:40:08 +1000

    Update address_space.cpp

--- src/core/address_space.cpp
+++ src/core/address_space.cpp
@@ -181,8 +181,6 @@ struct AddressSpace::Impl {
                 VirtualAlloc2(process, reinterpret_cast<PVOID>(virtual_addr), aligned_size,
                               MEM_RESERVE | MEM_COMMIT | MEM_REPLACE_PLACEHOLDER, prot, nullptr, 0);
         }
-        ASSERT_MSG(ptr, "{}", Common::GetLastErrorMsg());
-        return ptr;
     }
 
     void Unmap(VAddr virtual_addr, size_t size, bool has_backing) {
