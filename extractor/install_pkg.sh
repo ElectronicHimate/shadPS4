7b86636df2bfde4173698d29d4574c5a1d44eb66
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2026-02-27 04:36:29 +0000

    Revert "finish lpack"
    This reverts commit 3c0bea1ce006d1a7b756997d12410636e3a4d78d.

--- extractor/install_pkg.sh
+++ extractor/install_pkg.sh
@@ -1,40 +1,4 @@
 #!/bin/bash
 
-gamesDir="PATH_TO_GAMES_DIR"
-addonsDir="PATH_TO_ADDONS_DIR"
+echo $1
 
-if [ "$gamesDir" == "PATH_TO_GAMES_DIR" ]; then
-	echo You need to update gamesDir with the games path used by ShadPs4
-	echo "Press [enter] to close"
-	read
-	exit
-fi
-
-if [ "$addonsDir" == "PATH_TO_ADDONS_DIR" ]; then
-	echo You need to update addonsDir with the addons path used by ShadPs4
-	echo "Press [enter] to close"
-	read
-	exit
-fi
-
-`dirname $0`/pkg_extractor.AppImage $1 --check-type
-
-ret="$?"
-
-if [ "$ret" -eq 0 ]; then
-	echo "An error has occurred."
-else
-	if [ "$ret" -eq 101 ]; then
-		echo The file is a base game, installing to $gamesDir
-	elif [ "$ret" -eq 102 ]; then
-		echo The file is a game update, installing to $gamesDir
-	elif [ "$ret" -eq 103 ]; then
-		echo The file is a dlc, installing to $addonsDir
-		gamesDir=$addonsDir
-	fi
-	
-	`dirname $0`/pkg_extractor.AppImage $1 $gamesDir
-fi
-
-echo "Press [enter] to close"
-read
