393225f4464daf76451d825573085ad6d4c8f18d
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2026-02-27 04:36:50 +0000

    Revert "linux build"
    This reverts commit e1411a0b792799ca94d0bd208fb51df3d4bb4ca5.

--- src/common/assert.cpp
+++ src/common/assert.cpp
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: Copyright 2021 yuzu Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include <cstdio>
-#include <stdexcept>
 #include "common/arch.h"
 #include "common/assert.h"
 #include "common/logging/backend.h"
