b5562fd6b40a5113086729af52652dec9aaed2e2
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2026-04-06 11:58:13 +0400

    Change next_addr to 0x400000000

--- src/core/address_space.cpp
+++ src/core/address_space.cpp
@@ -130,7 +130,7 @@ struct AddressSpace::Impl {
                         supported_user_max);
         }
 
-        VAddr next_addr = SYSTEM_MANAGED_MIN; 
+        VAddr next_addr = 0x400000000ULL; 
         MEMORY_BASIC_INFORMATION info{};
 
         while (next_addr < 0x3800000000ULL) {
