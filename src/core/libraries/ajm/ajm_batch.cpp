dc0b9182c3ed6b01e4aec79d9575850236a8f5a0
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2026-04-06 10:38:55 +0000

    Revert "ajm: support for m4aac (#3880)"
    This reverts commit 9a3e4ea56b10e5ca02b644178b65abef4ddb25f8.

--- src/core/libraries/ajm/ajm_batch.cpp
+++ src/core/libraries/ajm/ajm_batch.cpp
@@ -165,7 +165,7 @@ AjmJob AjmStatisticsJobFromBatchBuffer(u32 instance_id, AjmBatchBuffer batch_buf
     ASSERT(job_flags.has_value());
     job.flags = job_flags.value();
 
-    AjmStatisticsJobFlags flags{.raw = job.flags.raw};
+    AjmStatisticsJobFlags flags(job.flags);
     if (input_control_buffer.has_value()) {
         AjmBatchBuffer input_batch(input_control_buffer.value());
         if (True(flags.statistics_flags & AjmStatisticsFlags::Engine)) {
