7d979a5af6deeacc098f8a1285e25404d4af4e4e
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2026-04-10 02:02:00 +0400

    Add supportedUserMax in config

--- src/common/config.h
+++ src/common/config.h
@@ -134,7 +134,7 @@ void setDevKitConsole(bool enable, bool is_game_specific = false);
 
 int getExtraDmemInMbytes();
 void setExtraDmemInMbytes(int value, bool is_game_specific = false);
-int getsupportedUserMax();
+uint64_t getsupportedUserMax();
 void setsupportedUserMax(uint64_t value, bool is_game_specific = false);
 bool getIsMotionControlsEnabled();
 void setIsMotionControlsEnabled(bool use, bool is_game_specific = false);
