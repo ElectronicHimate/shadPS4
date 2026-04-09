497f30625a655e0fec31e922c45273915c728238
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2026-04-09 20:31:58 +0400

    Coding style compliance

--- src/core/thread.cpp
+++ src/core/thread.cpp
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "libraries/kernel/threads/pthread.h"
+#include "core/libraries/kernel/threads/pthread.h"
 #include "thread.h"
 
 #ifdef _WIN64
