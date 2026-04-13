bbea21d5ebdb107cd5fe85766193ac4ab6fa9a3e
ElectronicHimate <107193409+ElectronicHimate@users.noreply.github.com>
2026-04-13 09:27:48 +0400

    •

--- src/core/emulator_settings.h
+++ src/core/emulator_settings.h
@@ -175,6 +175,7 @@ struct GeneralSettings {
     Setting<bool> neo_mode{false};
     Setting<bool> dev_kit_mode{false};
     Setting<int> extra_dmem_in_mbytes{0};
+    Setting<u64> supported_user_max{0x7000000000};
     Setting<bool> psn_signed_in{false};
     Setting<bool> trophy_popup_disabled{false};
     Setting<double> trophy_notification_duration{6.0};
@@ -197,6 +198,8 @@ struct GeneralSettings {
             make_override<GeneralSettings>("dev_kit_mode", &GeneralSettings::dev_kit_mode),
             make_override<GeneralSettings>("extra_dmem_in_mbytes",
                                            &GeneralSettings::extra_dmem_in_mbytes),
+            make_override<GeneralSettings>("supported_user_max",
+                                           &GeneralSettings::supported_user_max),
             make_override<GeneralSettings>("psn_signed_in", &GeneralSettings::psn_signed_in),
             make_override<GeneralSettings>("trophy_popup_disabled",
                                            &GeneralSettings::trophy_popup_disabled),
@@ -546,6 +549,7 @@ public:
     SETTING_FORWARD_BOOL(m_general, Neo, neo_mode)
     SETTING_FORWARD_BOOL(m_general, DevKit, dev_kit_mode)
     SETTING_FORWARD(m_general, ExtraDmemInMBytes, extra_dmem_in_mbytes)
+    SETTING_FORWARD(m_general, SupportedUserMax, supported_user_max)
     SETTING_FORWARD_BOOL(m_general, PSNSignedIn, psn_signed_in)
     SETTING_FORWARD_BOOL(m_general, TrophyPopupDisabled, trophy_popup_disabled)
     SETTING_FORWARD(m_general, TrophyNotificationDuration, trophy_notification_duration)
